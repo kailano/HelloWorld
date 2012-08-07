75069f551c5cb9d3f739a8e68aa8c6ef87d12f5e
kailano <clar0955@umn.edu>
2012-08-07 10:45:20 -0500

    Testing

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Kailin Clarke
+ Version     :
+ Copyright   : Copyright July 28, 2012 University of Minnesota.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+/*Comments*/
